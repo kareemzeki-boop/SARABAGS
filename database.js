05fac109845f575d0fb3935847c8ddd035d55e7d
Ze13ki <kareemzeki@gmail.com>
2026-04-23 13:37:53 +0400

    Add files via upload

--- database.js
+++ database.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const DATA_DIR = process.env.DATA_DIR || path.join(__dirname, '../data');
+const DATA_DIR = process.env.DATA_DIR || path.join(__dirname, 'data');
 if (!fs.existsSync(DATA_DIR)) fs.mkdirSync(DATA_DIR, { recursive: true });
 
 const files = {
